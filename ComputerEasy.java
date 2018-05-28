f4cbe2802849a4cb2ac7d72971199d2f56e78bc0
Szwajcii <d.szwajkos@gmail.com>
2018-05-28 13:21:51 +0200

    Implement new classes for computer players.

---
+++ ComputerEasy.java
@@ -0,0 +1,17 @@
+
+
+public class ComputerEasy extends ComputerPlayer {
+
+
+    public ComputerEasy(String name, int numberOfCards) {
+        super(name, numberOfCards);
+    }
+
+
+    @Override
+    public void makeMove() {
+        
+    }
+
+
+}
---
+++ ComputerHard.java
@@ -0,0 +1,17 @@
+
+
+public class ComputerHard extends ComputerPlayer {
+
+
+    public ComputerHard(String name, int numberOfCards) {
+        super(name, numberOfCards);
+    }
+
+
+    @Override
+    public void makeMove() {
+        
+    }
+
+
+}
