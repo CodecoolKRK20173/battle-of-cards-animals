5bd4fe2e3fc191c70287216c5a176e2e374f4edf
ejaworska <ewel.jaworska@gmail.com>
2018-05-29 15:43:18 +0200

    Implement getting cards from file

--- Deck.java
+++ Deck.java
@@ -1,4 +1,5 @@
 import java.util.*;
+import java.io.*;
 
 public class Deck {
     private ArrayList<Card> cards = new ArrayList<>();
@@ -10,39 +11,32 @@ public class Deck {
     private String[] names = new String[32];
 
     public Deck() {
-        cards.add(new Card("Jaguar", 40, 1.9, 120, 10, 20));
-        cards.add(new Card("Lion", 60, 2.5, 250, 20, 20));
-        cards.add(new Card("Leopard", 60, 1.9, 91, 8, 21));
-        cards.add(new Card("Toger", 50, 3, 320, 40, 26));
-        cards.add(new Card("lar gibbon", 55, 0.6, 7, 1, 40));
-        cards.add(new Card("Chimpanzee", 24, 1.2, 70, 6, 60));
-        cards.add(new Card("Orang-utan", 20, 1.4, 90, 12, 50));
-        cards.add(new Card("Western gorilla", 22, 1.7, 180, 22, 50));
-        cards.add(new Card("Brown bear", 40, 3, 780, 20, 46));
-        cards.add(new Card("Polar bear", 55, 3.4, 800, 50, 41));
-        cards.add(new Card("Sloth bear", 20, 1.8, 150, 6, 40));
-        cards.add(new Card("American black bear", 48, 1.9, 270, 10, 32));
-        cards.add(new Card("Red fox", 40, 0.9, 11, 1, 14));
-        cards.add(new Card("Arctic fox", 40, 0.6, 8, 0.75, 12));
-        cards.add(new Card("Grey wolf", 70, 1.5, 80, 10, 20));
-        cards.add(new Card("African wild dog", 56, 1.1, 36, 4.5, 12));
-        cards.add(new Card("Asian elephant", 24, 1.5, 5600, 150, 75));
-        cards.add(new Card("African elephant", 40, 5, 7000, 275, 70));
-        cards.add(new Card("Hippopotamus", 0, 2.7, 1500, 60, 50));
-        cards.add(new Card("Pygmy hippopotamus", 0, 1.6, 275, 25, 45));
-        cards.add(new Card("Red kangaroo", 63, 1.6, 40, 6, 20));
-        cards.add(new Card("Koala", 6, 0.8, 15, 0.5, 18));
-        cards.add(new Card("Duck-biffed platypus", 10, 0.6, 3, 1, 17));
-        cards.add(new Card("Short nosed echidna", 6, 0.45, 7, 15, 65));
-        cards.add(new Card("Moose / Elk", 54, 3.5, 700, 30, 20));
-        cards.add(new Card("Giraffe", 56, 5.3, 1500, 34, 30));
-        cards.add(new Card("Bongo", 36, 2.5, 400, 14, 19));
-        cards.add(new Card("Musk ox", 45, 2.3, 410, 6, 24));
-        cards.add(new Card("Blue whale", 48, 30, 140000, 3000, 80));
-        cards.add(new Card("Bowhead whale", 20, 14, 80000, 1500, 100));
-        cards.add(new Card("Gray whale", 10, 15, 34000, 1250, 70));
-        cards.add(new Card("Minkle whale", 34, 9.2, 10000, 500, 60));
-        getSpecifications();
+        getCardsFromFile("Cards.csv");
+    }
+
+    public void getCardsFromFile(String fileName) {
+        int nameIndex = 0;
+        int speedIndex = 1;
+        int lengthIndex = 2;
+        int weightIndex = 3;
+        int foodIndex = 4;
+        int lifeIndex = 5;
+        try {
+            Scanner sc = new Scanner(new File(fileName));
+            while(sc.hasNext()) {
+                String[] values = sc.nextLine().split(",");
+                String name = values[nameIndex];
+                double topSpeed = Double.parseDouble(values[speedIndex]);
+                double maxLength = Double.parseDouble(values[lengthIndex]);
+                double maxWeight = Double.parseDouble(values[weightIndex]);
+                double food = Double.parseDouble(values[foodIndex]);
+                double lifeSpan = Double.parseDouble(values[lifeIndex]);
+                cards.add(new Card(name, topSpeed, maxLength, maxWeight, food, lifeSpan));
+            }
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public ArrayList<Card> getAllCards() {
