4275c595277794a263868813a2a1b551ceaf5169
Szwajcii <d.szwajkos@gmail.com>
2018-05-30 09:38:39 +0200

    Implement new features.

--- GameHandler.java
+++ GameHandler.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Collections;
 
 
 
@@ -13,14 +14,13 @@ public class GameHandler {
 
     private int choice;
     private int numberOfHumanPlayers;
-
+    private int numberOfPlayers;
 
     private ArrayList<Player> players = new ArrayList<>();
 
-
     Display display = new Display();
+    Table table = new Table();
 
-    
 
     public void runApplication() {
 
@@ -41,38 +41,158 @@ public class GameHandler {
                     quit = true;
                     break;
             }
-        
         }
-
     }
 
-
+    
 
     public void startGame() {
         display.displayGameMode();
         int choice = getChoice();
         switch(choice) {
             case 1:
-                initPlayers(2);
+                int numberOfPlayers = chooseNumberOfPlayers();
+                createPlayerVsPlayerMode(numberOfPlayers);
                 break;
             case 2:
-                initPlayers(1);
+                numberOfPlayers = chooseNumberOfPlayers();
+                createPlayerVsComputerMode(numberOfPlayers);
                 break;
             case 3:
-                initPlayers(0);
+                numberOfPlayers = chooseNumberOfPlayers();
+                createComputerVsComputerMode(numberOfPlayers);
                 break;
-
         }
-
         playGame();
-
     }
 
 
     public void playGame() {
 
+        while(!gameOver()) {
+            // players.get(index).makeMove();
+        }
+
+    }
 
 
+    // public void switchPlayers() {
+
+    //     for(int i = 0; i < table.listOfPlayers.size(); i++){
+    //         if(table.listOfPlayers.get(i).getRoundWinner) {
+    //             table.listOfPlayers.get(i).chooseStatisticToCompare();
+    //         }
+    //     }
+
+    // }
+
+     
+    
+    public int getIndexOfMaxiumumElement() {
+        Collections.max(table.listOfCards);
+    }
+
+
+    public void compareTopSpeed() {
+        for(int i = 0; i < listOfCards; i++) {
+            listOfStatistic.add(listOfCards.get(i).getTopSpeed());
+        }
+    }
+
+
+    public void compareMaxLength() {
+        for(int i = 0; i < listOfCards; i++) {
+            listOfStatistic.add(listOfCards.get(i).getMaxLength());
+        }
+    }
+
+
+    public void compareMaxWeigth() {
+        for(int i = 0; i < listOfCards; i++) {
+            listOfStatistic.add(listOfCards.get(i).getMaxWeigth());
+        }
+    }
+
+
+    public void compareFood() {
+        for(int i = 0; i < listOfCards; i++) {
+            listOfStatistic.add(listOfCards.get(i).getFood());
+        }
+    }
+
+
+    public void compareLifeSpan() {
+        for(int i = 0; i < listOfCards; i++) {
+            listOfStatistic.add(listOfCards.get(i).getLifeSpan());
+        }
+    }
+    
+
+    public void addCardsToPlayersDeck() {
+        player = getRoundWinnerIndex();
+        for(int i = 0; i < listOfCards; i++) {
+            player.add(listOfCards.get(i));
+        }
+    }
+
+
+    public void clearListOfCards() {
+        for(int i = 0; i < listOfCards; i++) {
+            listOfCards.remove(i);
+        }
+    }
+
+
+    private int chooseNumberOfPlayers() {
+        System.out.print("Please choose number of players - 2, 3 or 4: ");
+        int numberOfPlayers = getChoice();
+        return numberOfPlayers;
+    }
+
+
+    private void createPlayerVsPlayerMode(int numberOfPlayers) {
+        for(int i = 0; i < numberOfPlayers; i++) {
+            players.add(createHumanPlayer());
+        }
+    }
+
+
+    private void createPlayerVsComputerMode(int numberOfPlayers) {
+        int numberOfHumanPlayers = 1;
+        players.add(createHumanPlayer());
+        for(int i = 0; i < numberOfPlayers - numberOfHumanPlayers; i++) {
+            players.add(createComputerPlayer("Computer " + i));
+        }
+    }
+
+
+    private void createComputerVsComputerMode(int numberOfPlayers) {
+        for(int i = 0; i < numberOfPlayers; i++) {
+            players.add(createComputerPlayer("Computer " + i));
+        }
+    }
+
+
+    public Player createHumanPlayer() {
+        System.out.print("Please enter player name: ");
+        String playerName = reader.next();
+        System.out.println("Player " + playerName + " created!");
+        return new HumanPlayer(playerName);
+    }
+
+
+    public Player createComputerPlayer(String computerName) {
+        display.displayGameLevel();
+        int choice = getChoice();
+        switch(choice) {
+            case 1:
+                return new ComputerEasy(computerName);
+            case 2:
+                return new ComputerHard(computerName);
+            default:
+                System.out.println("Please choose 1 or 2!");
+                return null;
+        }
     }
 
 
@@ -89,54 +209,7 @@ public class GameHandler {
         return isOver;
     }
 
-
-    private int chooseNumberOfPlayers() {
-
-
-    }
-
-
-    private void initPlayers(int numberOfHumanPlayers) {
-        if(numberOfHumanPlayers == 2) {
-            players.add(createHumanPlayer());
-            players.add(createHumanPlayer());
-        }
-        else if (numberOfHumanPlayers == 1) {
-            players.add(createHumanPlayer());
-            players.add(createComputerPlayer("Computer One"));
-        }
-        else {
-            players.add(createComputerPlayer("Computer One"));
-            players.add(createComputerPlayer("Computer Two"));
-        }
-    }
-
-
-
-
-
-    public Player createHumanPlayer() {
-        System.out.print("Please enter player name: ");
-        String playerName = reader.nextLine();
-        return new HumanPlayer(playerName);
-    }
-
-
-    public Player createComputPlayer(String computerName) {
-        display.displayGameLevel();
-        int choice = getChoice();
-        switch(choice) {
-            case 1:
-                return new ComputerEasy(computerName);
-            case 2:
-                return new ComputerHard(computerName);
-            default:
-                System.out.println("Please choose 1 or 2!");
-                return null;
-        }
-    }
     
-
 
     private int getChoice() {
         boolean stopWhile = true;
@@ -158,5 +231,9 @@ public class GameHandler {
         }
         return option;
     }
+
+
+
+    
 
 }
