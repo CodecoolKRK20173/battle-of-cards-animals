3e787399958cd61e53f4ec616f1eb08cfe86587a
ejaworska <ewel.jaworska@gmail.com>
2018-05-31 17:12:45 +0200

    Impement foolproofing

--- HumanPlayer.java
+++ HumanPlayer.java
@@ -4,19 +4,31 @@ public class HumanPlayer extends Player {
     Scanner scanner = new Scanner(System.in);
     Display display = new Display();
 
-
     public HumanPlayer(String name) {
         super(name);
     }
-
 
     @Override
     public int makeMove() {
         System.out.println("Player " + getName() + " make move!");
         System.out.println("Choose statistic to compare: ");
         display.displayCardStatistic();
-        int playerChoice = scanner.nextInt();
+        int playerChoice = getChoice();
         return playerChoice;
+    }
+
+    private int getChoice() {
+        boolean getNum = false;
+        int num = 0;
+        while(!getNum) {
+            System.out.print("Please write number (1,2,3,4,5):");
+            String choice = scanner.nextLine();
+            if(choice.equals("1") || choice.equals("2") || choice.equals("3") || choice.equals("4") || choice.equals("5")) {
+                num = Integer.valueOf(choice);
+                getNum = true;
+            }
+        }
+        return num;
     }
 
 }
